39a4b8782cecafdecdf3c2fe5bbf83a91e9d9d20
BrunoSprint <brunosprintdev@outlook.com>
2023-12-28 21:12:43 -0300

    .

---
+++ socket.py
@@ -0,0 +1,26 @@
+# Esta biblioteca fornece acesso de baixo nível à interface da rede.
+
+# O S.O fornece a API socket que relaciona o programa com a rede.
+
+#permite abertura e fechamento de conexões diretas (placa de rede)
+
+# Tornando possivél criar uma conexão entre um programa, uma placa de rede e um SO.
+
+
+# Desenvolvimento de um Cliente TCP.
+
+# O TCP ( Transmission Control Protocol) Protocolo de Controle de Transmissão é um dos protocolos
+#de comunicação que dão suporte a rede global Internet, verificando se os dados são
+# enviados na sequência correta e sem erros.
+
+# O programa verificará se os dados são enviados de maneira íntegra.
+
+
+
+
+# Desenvolvimento de um Cliente UDP
+
+# O UDP ( User Datagram Protocol) ou Protocolo de Datagrama de Usuário é um protocolo simples da camada de transporte que permite que a aplicação envie um Datagrama dentro de um pacote IPv4 ou IPv6 a um destino, porém sem qualquer tipo de garantia que o pacote chegue corretamente.
+
+
+# Criação de Servidor.
