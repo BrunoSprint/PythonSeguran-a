18d681b05a8fdc43597cd5b79b3b61f18bd27933
BrunoSprint <brunosprintdev@outlook.com>
2024-02-28 19:56:07 -0300

    .

---
+++ recursos3/verificador_IP_externo.py
@@ -0,0 +1,36 @@
+'''
+
+bibliotecas:
+
+re - Permite operações com expressões regulares.
+
+json - Fornece operação de codificação e decodificação JSON
+
+urllib.request import urlopen - Funções e classes que ajudam a abrir URLs
+
+http://ipinfo.io/json   checagem de ip.
+
+'''
+
+
+import re
+import json
+from urllib.request import urlopen
+
+
+url = 'https://ipinfo.io/json'
+
+resposta = urlopen(url)
+
+dados = json.load(resposta)
+
+ip = dados['ip']
+org = dados['org']
+cid = dados['city']
+pais = dados['country']
+regiao = dados['region']
+
+print('Detalhes do IP externo\n')
+print('IP: {4}\nRegião: {1}\nPaís: {2}\nCidade: {3}\nOrg.: {0}'.format(org,regiao,pais,cid,ip))
+
+
