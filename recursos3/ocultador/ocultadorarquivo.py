2b55353bac61f28fe6ee54aba819ffaea5c3180a
BrunoSprint <brunosprintdev@outlook.com>
2024-02-28 17:59:57 -0300

    .

--- recursos3/ocultador/ocultadorarquivo.py
+++ recursos3/ocultador/ocultadorarquivo.py
@@ -6,14 +6,33 @@ Desenvolvendo um Ocultador de arquivos.
 
 import ctypes
 
-pasta = input("Digite o Caminho da Pasta a ser Ocultada ex (C:/pasta): ")
+# Solicita ao usuário que digite o caminho da pasta que deseja ocultar
+pasta = input("Digite o Caminho da Pasta a ser Ocultada ex (C:/pasta): ")      #arquivo  ex:ocultar.txt
 
+# Define o atributo de ocultação de arquivos
 atributo_ocultar = 0x02
 
-retorno = ctypes.windll.kernel32.SetFileAttributesW(pasta, atributo_ocultar)   #'ocultar.txt'
+# Chama a função SetFileAttributesW da biblioteca ctypes.windll.kernel32 para ocultar a pasta
+retorno = ctypes.windll.kernel32.SetFileAttributesW(pasta, atributo_ocultar)
 
+# Verifica se a operação de ocultação foi bem-sucedida
 if retorno: 
-    print("pasta foi Ocultado")                #arquivo
+    print("pasta foi Ocultada")                #arquivo
 else:
-    print("pasta não Foi Ocultado")            #arquivo
+    print("pasta não Foi Ocultado")            #arquivo.
 
+'''
+
+Observações:
+
+    Importação da biblioteca ctypes: O código começa importando o módulo ctypes, que permite a chamada de funções da API do Windows a partir do Python.
+
+    Entrada do usuário para o caminho da pasta: Utiliza input() para solicitar ao usuário que forneça o caminho da pasta que deseja ocultar.
+
+    Definição do atributo de ocultação: O atributo de ocultação é definido como 0x02, que é um código para ocultar arquivos e pastas no Windows.
+
+    Chamada da função SetFileAttributesW: Utiliza ctypes.windll.kernel32.SetFileAttributesW() para definir os atributos da pasta, tornando-a oculta.
+
+    Verificação do retorno da função: Verifica se a função SetFileAttributesW() retorna um valor diferente de zero, indicando que a operação de ocultação foi bem-sucedida.
+
+    Mensagens de saída: Imprime mensagens indicando se a pasta foi ou não ocultada com sucesso.'''
